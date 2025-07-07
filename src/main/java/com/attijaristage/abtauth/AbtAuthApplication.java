ed3375fc8f04b1e9cb560fdb9bb90aa18dcf0dc2
BENJABER.ALAA <benjaber.alaa@attijaribank.com.tn>
2025-07-07 09:14:26 +0100

    Initial commit: Spring Boot Keycloak Auth Service setup

---
+++ src/main/java/com/attijaristage/abtauth/AbtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.attijaristage.abtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbtAuthApplication.class, args);
+	}
+
+}
